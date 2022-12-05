f7a2c7b03f402707bf3868ff0b5a7f453b9f70d7
YankinY <yankin.aa@mail.ru>
2022-12-05 16:38:41 +0300

    added normilze and reset css

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 /* @refresh reload */
 import { render } from 'solid-js/web';
-
 import './index.css';
+import 'normalize.css';
+import 'reset-css';
 import App from './App';
 
 render(() => <App />, document.getElementById('root') as HTMLElement);
